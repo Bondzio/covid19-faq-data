0e85786eb78166810fa6e432ac7858c5840946cb
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-04-30 09:53:34 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,13 +4,15 @@
   :license {:name "Eclipse Public License - v 2.0"
             :url  "http://www.eclipse.org/legal/epl-v20.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clj-http "3.10.1"]
-                 ;; [org.martinklepsch/clj-http-lite "0.4.3"]
                  [hickory "0.7.1"]
                  [cheshire "5.10.0"]
                  [hiccup "1.0.5"]
                  [clojure.java-time "0.3.2"]
-                 [borkdude/babashka "0.0.75"]]
+                 [borkdude/babashka "0.0.75"]
+                 [metosin/pohjavirta "0.0.1-alpha7"]
+                 [metosin/jsonista "0.2.5"]
+                 [metosin/reitit      "0.4.2"]
+                 [metosin/reitit-ring "0.4.2"]]
   :clean-targets ^{:protect false} ["target" "public"]
   :aliases {"run" ["trampoline" "run" "-m" "core"]}
   )
