d88e27c08395988f37f14efe14b507305460ec8b
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-05-02 11:08:02 +0200

    src/core.clj: Store hits

--- src/core.clj
+++ src/core.clj
@@ -4,15 +4,21 @@
             [reitit.ring :as ring]
             [java-time :as t]
             [chime.core :as chime]
+            [data :as data]
             [chime.core-async :refer [chime-ch]]
             [clojure.core.async :as async]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.walk :as walk]
+            [reitit.ring.middleware.parameters :as parameters]
+            [ring.middleware.cors :refer [wrap-cors]]))
 
-(def dev? true)
+(defonce dev? true)
+(defonce port 3000)
+(defonce front-url "http://localhost:9500")
 
 (def questions-ids
   (->> (j/read-value
-        (slurp "docs/faq-questions.json"))
+        (slurp (str data/upload-dir "faq-questions.json")))
        (map #(select-keys % ["i"]))
        (map vals)
        flatten))
@@ -28,6 +34,7 @@
  stats :backup
  (fn [_ a _ _]
    (when (zero? (mod (apply + (map :hits (vals @a))) 10))
+     (when dev? (println "Saving to stats.edn..."))
      (spit "stats.edn" (pr-str @a)))))
 
 (def valid-tokens (atom {}))
@@ -40,44 +47,66 @@
                                   (t/instant (val (first %)))))
                     @valid-tokens))))
 
+(defn wrap-headers [m]
+  (assoc m :headers {"Content-Type" "application/json"}))
+
 (defn get-token [_]
   (let [token (str (java.util.UUID/randomUUID))
         date  (str (t/instant))]
     (do (swap! valid-tokens conj {token date})
-        {:status 200
-         :body   (j/write-value-as-string
-                  {:token token :date date})})))
+        (wrap-headers
+         {:status 200
+          :body   (j/write-value-as-string {:token token})}))))
 
 (defn get-stats [_]
-  {:status 200
-   :body   (j/write-value-as-string
-            (map (fn [[k v]]
-                   (into {} (concat {:i k} v)))
-                 @stats))})
+  (wrap-headers
+   {:status 200
+    :body   (j/write-value-as-string
+             (map (fn [[k v]]
+                    (into {} (concat {:i k} v)))
+                  @stats))}))
 
 ;; Reject answers when visit is < 5 secondes or > 1 hour
-(defn valid-date? [date-token date-answer]
+(defn valid-date? [date-token]
   (let [dt     (t/instant date-token)
-        da     (t/instant date-answer)
+        da     (t/instant)
         dt+10  (t/plus dt (t/seconds 5))
         dt+600 (t/plus dt (t/seconds 3600))]
     (and (= da (t/max dt+10 da))
          (= dt+600 (t/max dt+600 da)))))
 
-(defn hit [{{:keys [id token date]} :body-params}]
-  (if-let [date-token (get @valid-tokens token)]
-    (if (valid-date? date-token date)
-      (do (swap! stats #(update-in % [id :hits] inc))
-          {:status 200})
-      {:status 400})
-    {:status 400}))
+(defn prn-resp [status msg]
+  (when dev?
+    (wrap-headers
+     {:status status
+      :body   (j/write-value-as-string
+               {:response msg})})))
+
+(defn hit [{params :query-params}]
+  (let [params
+        (walk/keywordize-keys params)
+        {:keys [id token]} params]
+    (if-let [date-token (get @valid-tokens token)]
+      (if (valid-date? date-token)
+        (do (swap! stats #(update-in % [id :hits] inc))
+            (prn-resp 200 "All good"))
+        (prn-resp 400 "Invalid token"))
+      (prn-resp 400 "Token not found"))))
 
 (def app
   (ring/ring-handler
    (ring/router
     [["/token" {:get get-token}]
      ["/stats" {:get get-stats}]
-     ["/hit" {:post hit}]])))
+     ["/hit" {:get hit}]])
+   (ring/create-default-handler)
+   {:middleware
+    [parameters/parameters-middleware
+     #(wrap-cors
+       %
+       :access-control-allow-origin [(re-pattern front-url)]
+       :access-control-allow-headers ["Content-Type"]
+       :access-control-allow-methods [:get :post])]}))
 
 (defn start-tokens-purge-loop []
   (let [chimes (chime-ch (chime/periodic-seq
@@ -87,20 +116,9 @@
                    (purge-tokens)
                    (recur))))))
 
-;; Testing
-(comment
-  (app {:request-method :post
-        :uri            "/hit"
-        :body-params    {:id    "uneid"
-                         :token "751d2e65-c1e3-42d0-b38a-92320d623bf4"
-                         :date  "2020-05-01T09:35:08.600263Z"}})
-  (app {:request-method :get
-        :uri            "/token"})
-  (app {:request-method :get
-        :uri            "/stats"})
-  )
-
-(defn -main []
-  (if-not dev? (start-tokens-purge-loop))
-  (-> app (server/create {:port 3000}) server/start))
-
+(defn -main [& [json]]
+  (if json
+    (data/generate-json)
+    (do (if-not dev? (start-tokens-purge-loop))
+        (-> app (server/create {:port port}) server/start)
+        (println "API started on localhost:3000"))))
