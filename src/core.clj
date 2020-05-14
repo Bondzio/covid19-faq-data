f7a850b7d21f6ad098cab77bdff4adcb8e309043
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-05-14 07:30:01 +0200

    Allow post queries and use muuntaja as middleware

--- src/core.clj
+++ src/core.clj
@@ -9,9 +9,12 @@
             [clojure.core.async :as async]
             [clojure.edn :as edn]
             [clojure.walk :as walk]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [ring.middleware.params :as params]
+            [muuntaja.core :as m]
             [reitit.ring.middleware.parameters :as parameters]
             [ring.middleware.cors :refer [wrap-cors]]
-            [mount.core :refer [defstate] :as mount]))
+            [mount.core :as mount]))
 
 (defonce dev? false)
 (defonce port 3000)
@@ -32,8 +35,7 @@
 
 (def stats
   (let [faq-ids
-        (->> (map (fn [a] {a {:h 0 :n {:c 0 :m 0}}})
-                  questions-ids)
+        (->> (map (fn [a] {a {:h 0 :n {:c 0 :m 0}}}) questions-ids)
              (into {}))
         stats-edn
         (edn/read-string
@@ -71,23 +73,18 @@
                                        (t/instant (last %))))
                          @valid-tokens)))
           (println "Tokens purged"))
-      (println "Pas de token"))))
-
-(defn wrap-headers [m]
-  (assoc m :headers {"Content-Type" "application/json"}))
+      (println "No token to purge yet"))))
 
 (defn get-token [_]
   (let [token (str (java.util.UUID/randomUUID))
         date  (str (t/instant))]
     (swap! valid-tokens conj {token date})
-    (wrap-headers
-     {:status 200
-      :body   (j/write-value-as-string {:token token})})))
+    {:status 200
+     :body   {:token token}}))
 
 (defn get-stats [_]
-  (wrap-headers
-   {:status 200
-    :body   (j/write-value-as-string @stats)}))
+  {:status 200
+   :body   @stats})
 
 (defn valid-date? [date-token]
   (let [dt     (t/instant date-token)
@@ -98,10 +95,8 @@
          (= dt+max (t/max dt+max da)))))
 
 (defn prn-resp [status msg]
-  (wrap-headers
-   {:status status
-    :body   (j/write-value-as-string
-             {:response msg})}))
+  {:status status
+   :body   {:response msg}})
 
 (defn hit [{params :query-params}]
   (let [params
@@ -118,35 +113,34 @@
   (float (/ (+ (* old-mean old-cnt) note)
             (inc old-cnt))))
 
-(defn note [{params :query-params}]
-  (let [params
-        (walk/keywordize-keys params)
-        {:keys [id token note]} params]
-    (if-let [date-token (get @valid-tokens token)]
-      (if (valid-date? date-token)
-        (let [cnt  (get-in @stats [id :n :c])
-              note (edn/read-string note)]
-          (do (swap! stats update-in [id :n :c] inc)
-              (swap! stats update-in [id :n :m]
-                     #(mean % cnt note))
-              (prn-resp 200 "OK")))
-        (prn-resp 400 "Invalid token"))
-      (prn-resp 400 "Token not found"))))
+(defn note [{{:keys [id token note]} :body-params}]
+  (if-let [date-token (get @valid-tokens token)]
+    (if (valid-date? date-token)
+      (let [cnt  (get-in @stats [id :n :c])
+            note (edn/read-string note)]
+        (swap! stats update-in [id :n :c] inc)
+        (swap! stats update-in [id :n :m] #(mean % cnt note))
+        (prn-resp 200 "OK"))
+      (prn-resp 400 "Invalid token"))
+    (prn-resp 400 "Token not found")))
 
 (def handler
   (ring/ring-handler
    (ring/router
     [["/token" {:get get-token}]
      ["/stats" {:get get-stats}]
-     ["/note" {:get note}]
-     ["/hit" {:get hit}]])
+     ["/note" {:post note}]
+     ["/hit" {:get hit}]]
+    {:data {:muuntaja   m/instance
+      	    :middleware [params/wrap-params
+                         muuntaja/format-middleware]}})
    (ring/create-default-handler)
    {:middleware
     [parameters/parameters-middleware
      #(wrap-cors
        %
        :access-control-allow-origin [front-url]
-       :access-control-allow-methods [:get])]}))
+       :access-control-allow-methods [:get :post])]}))
 
 (defn start-tokens-purge-loop []
   (let [chimes (chime-ch (chime/periodic-seq
@@ -156,10 +150,10 @@
         (purge-tokens)
         (recur)))))
 
-(defstate server
+(mount/defstate s
   :start (do (println "API started on localhost:3000")
              (server/run-server handler {:port 3000}))
-  :stop (when server (server :timeout 100)))
+  :stop (when s (s :timeout 100)))
 
 (defn -main [& [json]]
   (if json ;; Any value is OK
@@ -169,6 +163,6 @@
         (mount/start))))
 
 (comment
-  (-main)
+  (mount/start)
   (mount/stop)
   )
